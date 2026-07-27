72aaecc0220ce44d94aca6522785ce6cb3a7b775
Alperen Atar <mahmutalperenatar18@gmail.com>
2026-07-27 17:35:15 +0300

    Portfolyo sitesi ilk sürüm

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// src/config.js
+// Backend adresi TEK yerden yönetilir.
+// Apps Script'te yeni dağıtım açıp URL değişirse SADECE burayı güncelle.
+export const GAS_BASE = "https://script.google.com/macros/s/AKfycbyAXGvbkdV3bgeP13Japf2UdAyA5BUwcutShIlSkZibyfSQOUdzNvUIYbTeYtNp23I/exec";
