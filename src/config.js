0f8fd68595ec4089df180a329371f58591f07d38
Alperen Atar <mahmutalperenatar18@gmail.com>
2026-07-28 10:21:58 +0300

    yetkinlik&belge yukleme eklendi

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 // Backend adresi TEK yerden yönetilir.
 // Apps Script'te yeni dağıtım açıp URL değişirse SADECE burayı güncelle.
-export const GAS_BASE = "https://script.google.com/macros/s/AKfycbyAXGvbkdV3bgeP13Japf2UdAyA5BUwcutShIlSkZibyfSQOUdzNvUIYbTeYtNp23I/exec";
+export const GAS_BASE = "https://script.google.com/macros/s/AKfycbwMX3p5N3VuYl5Ou6CPDp6p-sXg2QiTMjqnniwAWgrQRg--XBX5Zy9r-ryY_wp8mNJd/exec";
