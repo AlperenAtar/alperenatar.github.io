156a2d7b8bff89924ce4bd7b1d7a9dc5b7c0121b
Alperen Atar <mahmutalperenatar18@gmail.com>
2026-07-28 10:30:34 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 // Backend adresi TEK yerden yönetilir.
 // Apps Script'te yeni dağıtım açıp URL değişirse SADECE burayı güncelle.
-export const GAS_BASE = "https://script.google.com/macros/s/AKfycbx5BO1h3Zw-AkJ3rwyNEdvsIEe8cqr2tjvcThRsSm8/exec";
+export const GAS_BASE = "https://script.google.com/macros/s/AKfycbxvhW3Tar3XATaBLKTzXC6RwAIn2gpIRYa09Q2OKv_WGzGFOSA4m14pmjIg06DqsAMY/exec";
